0ca58bf851916aac751690347de7a1569a4457a7
Kaushik Saha <kaushiksaha004@gmail.com>
2025-08-07 00:49:18 +0530

    assign and unassign slot

--- internal/service/slot_assignment/service.go
+++ internal/service/slot_assignment/service.go
@@ -87,9 +87,73 @@ func (sas *SlotAssignmentService) AutoAssignSlot(ctx context.Context, vehicleId 
 }
 
 func (sas *SlotAssignmentService) UnassignSlot(ctx context.Context, vehicleId string) error {
-	panic("not implemented") // TODO: Implement
+	ctxUser := ctx.Value(constants.User).(userjwt.UserJwt)
+
+	uid, err := uuid.Parse(ctxUser.ID)
+	if err != nil {
+		return err
+	}
+	userVehicles, err := sas.vehicleRepo.GetVehiclesByUserId(uid)
+	if err != nil {
+		return err
+	}
+
+	vehicleUuid, err := uuid.Parse(vehicleId)
+	if err != nil {
+		return err
+	}
+	newVehicle, err := sas.vehicleRepo.GetVehicleById(vehicleUuid)
+	if err != nil {
+		return err
+	}
+
+	cnt := 0
+	for _, val := range userVehicles {
+		if val.AssignedSlot.SlotNumber == newVehicle.AssignedSlot.SlotNumber {
+			cnt++
+		}
+	}
+
+	if cnt == 1 {
+		err = sas.slotRepo.Save(slot.Slot{
+			BuildingId:  newVehicle.AssignedSlot.BuildingId,
+			FloorNumber: newVehicle.AssignedSlot.FloorNumber,
+			SlotNumber:  newVehicle.AssignedSlot.SlotNumber,
+			IsOccupied:  false,
+			SlotType:    newVehicle.AssignedSlot.SlotType,
+		})
+		if err != nil {
+			return err
+		}
+
+	}
+
+	newVehicle.AssignedSlot = slot.Slot{}
+	err = sas.vehicleRepo.Save(newVehicle)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (sas *SlotAssignmentService) AssignSlot(ctx context.Context, vehicleId string, slot slot.Slot) error {
-	panic("not implemented") // TODO: Implement
+	// ctxUser := ctx.Value(constants.User).(userjwt.UserJwt)
+
+	vehicle, err := sas.vehicleRepo.GetVehicleById(uuid.MustParse(vehicleId))
+	if err != nil {
+		return err
+	}
+
+	userVehicles, err := sas.vehicleRepo.GetVehiclesByUserId(vehicle.UserId)
+	if err != nil {
+		return err
+	}
+
+	for i, val := range userVehicles {
+		if val.VehicleType == vehicle.VehicleType {
+			userVehicles[i].AssignedSlot = slot
+			sas.vehicleRepo.Save(userVehicles[i])
+		}
+	}
+	return sas.slotRepo.Save(slot)
 }
